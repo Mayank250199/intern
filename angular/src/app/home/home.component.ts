6ab804d860e9805a247f976ff26e45aa143511bc
Mayank Sharma <mayanksharma250199@gmail.com>
2018-06-29 02:02:04 +0530

    Done!!

--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
 
+declare var $:any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,13 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit() {
+  $(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            400:{
+                items:2
+            },
+            500:{
+                items:3
+            }
+        }
+    })
+
+  })
   }
   myInterval = 1500;
    activeSlideIndex = 0;
-   
+
     slides = [
       {image: '../../../../assets/images/3.jpg'},
       {image: '../../../../assets/images/67.jpg'},
