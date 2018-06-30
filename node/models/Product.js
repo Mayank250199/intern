c3102698a604dff39e52920c9e082346784023a6
Mayank Sharma <mayanksharma250199@gmail.com>
2018-06-30 23:18:15 +0530

    Minor

---
+++ node/models/Product.js
@@ -0,0 +1,41 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+var storage = new keystone.Storage({
+	adapter: keystone.Storage.Adapters.FS,
+	fs: {
+		path: 'uploads/product',
+		publicPath: '/uploads/product/',
+	},
+	schema: {
+    originalname: true,
+    url: true,
+  },
+});
+
+var storageslider = new keystone.Storage({
+	adapter: keystone.Storage.Adapters.FS,
+	fs: {
+		path: 'uploads/product/slider',
+		publicPath: '/uploads/product/slider',
+	},
+	schema: {
+    originalname: true,
+    url: true,
+  },
+});
+var Product = new keystone.List('Product');
+
+Product.add({
+	Productname: { type: Types.Text, initial: true, required: true, min: 0, max:30 },
+  images:{ type: Types.File, initial: true, required: true, storage: storage },
+  description: { type: Types.Text, initial: true, required: true, min: 0, max:100 },
+  sliderimage:{ type: Types.File, initial: true, required: true, storage: storageslider},
+});
+
+
+/**
+ * Registration
+ */
+Product.defaultColumns = 'Productname,images,description,sliderimage';
+Product.register();
