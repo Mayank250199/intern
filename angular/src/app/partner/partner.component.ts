514af091d5b16f518e599cfeea62af604e5b6a67
Mayank Sharma <mayanksharma250199@gmail.com>
2018-06-30 17:37:39 +0530

    Done!!

--- angular/src/app/partner/partner.component.ts
+++ angular/src/app/partner/partner.component.ts
@@ -10,6 +10,32 @@ export class PartnerComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function(){
+      $('.owl-carousel').owlCarousel({
+          loop:true,
+          margin:10,
+          nav:false,
+          dots:false,
+          responsive:{
+              0:{
+                  items:1
+              },
+              400:{
+                  items:2
+              },
+              500:{
+                  items:4
+              }
+          }
+      })
+
+    })
   }
+
+  slides = [
+    {image: '../../../../assets/images/3.jpg'},
+    {image: '../../../../assets/images/67.jpg'},
+    {image: '../../../../assets/images/78.jpg'}
+  ];
 
 }
